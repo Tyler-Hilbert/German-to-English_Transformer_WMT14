76e924c3915608ac60bf0527e271bfc44ff06faa
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2025-03-04 08:28:42 -0800

    Removed validation loss and save model every 10 epochs instead of every epoch

--- Reference/ArjunSarkar_BuildingYourOwnTransformerFromScratch/Main.py
+++ Reference/ArjunSarkar_BuildingYourOwnTransformerFromScratch/Main.py
@@ -54,51 +54,49 @@ def trim_tokens_to_fit(tokens, num_sequences, max_seq_length):
 #   EN tokens
 en_tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 en_tokens_train = tokenize('data/data_en_train.txt', en_tokenizer, en_end_token, max_seq_length)
-en_tokens_validation = tokenize('data/shuffled_en.txt', en_tokenizer, en_end_token, max_seq_length)
 #   DE tokens
 de_tokenizer = AutoTokenizer.from_pretrained("bert-base-german-cased")
 de_tokens_train = tokenize('data/data_de_train.txt', de_tokenizer, de_end_token, max_seq_length)
-de_tokens_validation = tokenize('data/shuffled_de.txt', de_tokenizer, de_end_token, max_seq_length)
 
 # Trim token to fit (and in this case to reduce training set size)
 # Convert from list to tensor
 en_tensor_train = trim_tokens_to_fit(en_tokens_train, num_sequences, max_seq_length)
-en_tensor_validation = trim_tokens_to_fit(en_tokens_validation, num_sequences, max_seq_length)
 de_tensor_train = trim_tokens_to_fit(de_tokens_train, num_sequences, max_seq_length)
-de_tensor_validation = trim_tokens_to_fit(de_tokens_validation, num_sequences, max_seq_length)
 
 # Model
 transformer = Transformer(vocab_size, vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout)
 criterion = nn.CrossEntropyLoss(ignore_index=0)
 optimizer = optim.Adam(transformer.parameters(), lr=lr, betas=(0.9, 0.98), eps=1e-9)
 
-# Training Loop with Validation Loss
+# Train
+transformer.train()
 for epoch in range(epochs):
-    transformer.train()
     optimizer.zero_grad()
-
-    # Forward pass
     output = transformer(en_tensor_train, de_tensor_train[:, :-1])
     loss = criterion(output.contiguous().view(-1, vocab_size), de_tensor_train[:, 1:].contiguous().view(-1))
-
-    # Backpropagation
     loss.backward()
     optimizer.step()
+    print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
 
-    # Compute loss
-    transformer.eval()
-    with torch.no_grad():
-        val_output = transformer(en_tensor_validation, de_tensor_validation[:, :-1])
-        val_loss = criterion(val_output.contiguous().view(-1, vocab_size), de_tensor_validation[:, 1:].contiguous().view(-1))
-    print(f"Epoch: {epoch+1}, Training Loss: {loss.item()}, Validation Loss: {val_loss.item()}")
+    # Save model
+    if epoch % 10 == 0:
+        full_model_path = model_path + '_epoch' + str(epoch) + '.pt'
+        torch.save({
+                'epoch': epoch,
+                'model_state_dict': transformer.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': loss
+            },
+            full_model_path
+        )
 
-    # Save Model
-    full_model_path = model_path + '_epoch' + str(epoch) + '.pt'
-    torch.save({
-            'epoch': epoch,
-            'model_state_dict': transformer.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': loss
-        },
-        full_model_path
-    )
+# Save final model
+full_model_path = model_path + '_epoch' + str(epoch) + '.pt'
+torch.save({
+        'epoch': epoch,
+        'model_state_dict': transformer.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': loss
+    },
+    full_model_path
+)
