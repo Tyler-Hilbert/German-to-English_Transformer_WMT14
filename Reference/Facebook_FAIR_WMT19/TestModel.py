3ac662e1541dde9ad0e6725f4dbe684a7e0bad35
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2025-03-14 21:50:03 -0700

    Improved tokenization for BLEU score

--- Reference/Facebook_FAIR_WMT19/TestModel.py
+++ Reference/Facebook_FAIR_WMT19/TestModel.py
@@ -4,11 +4,11 @@ from transformers import FSMTForConditionalGeneration, FSMTTokenizer
 from nltk.translate.bleu_score import sentence_bleu
 
 # Dataset
-validation_dataset_path_input = '../data/data_en_validation.txt'
-validation_dataset_path_expected_outputs = '../data/data_de_validation.txt'
+validation_dataset_path_input = '../data/data_de_validation.txt'
+validation_dataset_path_expected_outputs = '../data/data_en_validation.txt'
 
 # Load model and tokenizer
-mname = "facebook/wmt19-en-de"
+mname = "facebook/wmt19-de-en"
 tokenizer = FSMTTokenizer.from_pretrained(mname)
 model = FSMTForConditionalGeneration.from_pretrained(mname)
 
@@ -24,15 +24,21 @@ with open(validation_dataset_path_expected_outputs, 'r') as file:
 
 # Run validation data
 print (f'Testing dataset at {validation_dataset_path_input}:')
-print ('format: [input] [output] [BLEU score]')
+print ('format: [expected_output] [output] [BLEU score]')
 print ('----------------------------------------------')
 for input, expected_output in zip(inputs, expected_outputs):
         input_ids = tokenizer.encode(input, return_tensors="pt")
         outputs = model.generate(input_ids)
         decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        bleu_score = sentence_bleu(input.strip(), expected_output.strip()) # TODO - Is there a better way to score?
-        
-        print (input)
+
+        print (expected_output)
         print (decoded)
+
+        # TODO - better tokenizer
+        decoded = decoded.split(' ')
+        expected_output = expected_output.split(' ')
+
+        # Score
+        bleu_score = sentence_bleu(decoded, expected_output)
         print (bleu_score)
         print ('')
